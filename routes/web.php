c10fd205ffd4e4bab851336ad6a80de9f4744f31
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2024-12-20 02:04:19 +0530

    saved

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StudentController;
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -13,4 +14,3 @@ Route::get('/edit-student/{id}',[StudentController::class,'loadEditStudentForm']
 Route::get('/delete-student/{id}',[StudentController::class,'deleteStudent']);
 Route::post('/set-student',[StudentController::class,'addStudent'])->name('addStudent');
 Route::post('/update-student',[StudentController::class,'editStudent'])->name('editStudent');
-
